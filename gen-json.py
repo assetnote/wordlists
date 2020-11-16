4c47bd9878464b5436c8d0acc33c213007e92c1a
Shubs <sshah@assetnote.io>
2020-11-17 08:16:40 +1100

    feat(table): sorting on date and file size

--- gen-json.py
+++ gen-json.py
@@ -7,8 +7,8 @@ now = datetime.now()
 directory = sys.argv[1]
 filelist = os.listdir(directory)
 
-def sizeof_fmt(num, suffix='B'):
-    for unit in ['','Ki','Mi','Gi','Ti','Pi','Ei','Zi']:
+def sizeof_fmt(num, suffix='b'):
+    for unit in ['','k','m','g','t','p','e','z']:
         if abs(num) < 1024.0:
             return "%3.1f%s%s" % (num, unit, suffix)
         num /= 1024.0
@@ -16,7 +16,7 @@ def sizeof_fmt(num, suffix='B'):
 
 def modification_date(filename):
     t = os.path.getmtime(filename)
-    return datetime.fromtimestamp(t)
+    return t
 
 files_json = {"data":[]}
 
@@ -24,7 +24,7 @@ for item in filelist:
 	full_path = "{}/{}".format(directory, item)
 	num_lines = sum(1 for line in open(full_path))
 	file_size = sizeof_fmt(os.path.getsize(full_path))
-	modification_time = modification_date(full_path).strftime("%d/%m/%Y %H:%M:%S")
+	modification_time = modification_date(full_path)
 	obj = OrderedDict([("Filename", item), ("Line Count", num_lines), ("File Size", file_size), ("Date", modification_time), ("Download", "<a href='data/automated/{0}'>Download</a>".format(item))])
 	files_json["data"].append(obj)
 
